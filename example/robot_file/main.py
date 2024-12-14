ef3a8efd7c855b990bdf72a4e177b4dda841bc29
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2024-12-14 15:25:54 +0900

    💡IMPROVE:

--- example/robot_file/main.py
+++ example/robot_file/main.py
@@ -1,11 +1,30 @@
 import numpy as np
 
-from robokots.robot_model import RobotStruct
-from robokots.robot_io import *
+from robokots.robot import *
 
 def main():
-  robot = RobotStruct.from_json_file("sample_robot.json")
-  robot.print_structure()
+  robot = Robot.from_json_file("sample_robot.json")
+  robot.robot.print_structure()
+  
+  coord = [1., -2., 1.]
+  veloc = [0., 0., 0.]
+  accel = [0., 0., 0.]
+  force = [0., 0., 0.]
+  
+  vec = []
+  vec.extend(coord)
+  vec.extend(veloc)
+  vec.extend(accel)
+  vec.extend(force)
+  
+  robot.import_motions(vec)
+  print(robot.motions.motions)
+  
+  robot.kinematics()
+  
+  print(robot.state.state_df.df)
+
+  robot.show_robot()
   
 if __name__ == "__main__":
     main()
