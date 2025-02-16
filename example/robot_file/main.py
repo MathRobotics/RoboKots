7f43895f44a05cb5761471ee3de48f52bfa0864d
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-02-16 21:45:14 +0900

    💡IMPROVE:

--- example/robot_file/main.py
+++ example/robot_file/main.py
@@ -8,11 +8,11 @@ def main():
   
   link_name_list = ["world","base","arm1","arm2","arm3"]
   links = robot.link_list(link_name_list)
-  print(list(l.name for l in links) )
+  print(list(l.type for l in links) )
 
   joint_name_list = ["root","joint1","joint2","joint3"]
   joints = robot.joint_list(joint_name_list)
-  print(list(j.name for j in joints) )
+  print(list(j.type for j in joints) )
   
   coord = [1., -1., 1.]
   veloc = [0., 0., 0.]
