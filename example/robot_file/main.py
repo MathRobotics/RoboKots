1cb4bd2d3adde454d80eece0d85ebae6da506928
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-02-16 20:57:09 +0900

    🌟ADD:

--- example/robot_file/main.py
+++ example/robot_file/main.py
@@ -6,9 +6,13 @@ def main():
   robot = Robot.from_json_file("sample_robot.json")
   robot.print_structure()
   
-  link_name_list = ["world", "base","arm1","arm2","arm3"]
+  link_name_list = ["world","base","arm1","arm2","arm3"]
   links = robot.link_list(link_name_list)
   print(list(l.name for l in links) )
+
+  joint_name_list = ["root","joint1","joint2","joint3"]
+  joints = robot.joint_list(joint_name_list)
+  print(list(j.name for j in joints) )
   
   coord = [1., -1., 1.]
   veloc = [0., 0., 0.]
