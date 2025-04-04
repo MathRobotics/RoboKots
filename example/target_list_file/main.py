88723052e8b8184c80eae987323fb9f5858f0dec
Taiki Ishigaki <taiki.ishigaki@ieee.org>
2025-04-04 17:29:02 +0900

    📦 NEW: example for link_jacob_target

--- example/target_list_file/main.py
+++ example/target_list_file/main.py
@@ -8,6 +8,23 @@ def main():
   
     robot.set_target_from_file("target_list.json")
     robot.print_targets()
+
+    coord = [1., -1., 1.]
+    veloc = [0., 0., 0.]
+    accel = [0., 0., 0.]
+    force = [0., 0., 0.]
+    
+    vec = []
+    vec.extend(coord)
+    vec.extend(veloc)
+    vec.extend(accel)
+    vec.extend(force)
+    
+    robot.import_motions(vec)
+    
+    robot.kinematics()
+
+    print(robot.link_jacobian_target())
   
 if __name__ == "__main__":
     main()
