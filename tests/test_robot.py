b12612aa6b25ed63f6d7e8d2c128f5027a7d27a7
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-04-15 19:49:04 +0900

    📦 NEW: joint struct init test

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -22,3 +22,21 @@ def test_robot_struct_init():
     assert robot.link_dof == 0
     assert robot.link_names == ["link1","link2"]
     assert robot.joint_names == ["joint1"]
+
+def test_joint_struct_init():
+    origin = SE3.rand()
+    # Create a mock joint
+    joint = JointStruct(0, "joint1", "revolute", np.array((0,0,1)), 0, 1, origin)
+    
+    # Check if the joint is initialized correctly
+    assert joint.name == "joint1"
+    assert joint.type == "revolute"
+    assert np.array_equal(joint.axis, np.array((0,0,1)))
+    assert joint.parent_link_id == 0
+    assert joint.child_link_id == 1
+    assert joint.dof == 1
+    assert joint.dof_index == 0
+    assert np.allclose(joint.select_mat, np.array([[0], [0], [1], [0], [0], [0]]))
+    assert joint.select_indeces == [2]
+    assert isinstance(joint.origin, SE3)
+    assert np.allclose(joint.origin.mat(), origin.mat())
