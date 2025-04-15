96ca4c0dbf899172bf30d94d1596799880bfaf0d
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-04-15 18:13:59 +0900

    📦 NEW:

---
+++ example/cmtm_jacob/main.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from robokots.robot import *
+
+def main():
+    robot = Robot.from_json_file("../model/sample_robot.json")
+    robot.print_structure()
+  
+    robot.set_target_from_file("target_list.json")
+    robot.print_targets()
+
+    coord = [1., -1., 1.]
+    veloc = [1., 2., 3.]
+    accel = [0., 0., 0.]
+    
+    vec = []
+    vec.extend(coord)
+    vec.extend(veloc)
+    vec.extend(accel)
+    
+    robot.import_motions(vec)
+    
+    robot.kinematics()
+
+    print(robot.link_cmtm_jacobian_target())
+  
+if __name__ == "__main__":
+    main()
