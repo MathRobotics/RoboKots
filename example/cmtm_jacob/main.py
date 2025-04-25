609d0be04212cd8376eecf9a2e3dc8bb396d5536
Taiki Ishigaki <taiki000ishigaki@gmail.com>
2025-04-25 20:03:30 +0900

    👌 IMPROVE:

--- example/cmtm_jacob/main.py
+++ example/cmtm_jacob/main.py
@@ -1,25 +1,51 @@
 import numpy as np
 
+import mathrobo as mr
 from robokots.kots import *
 import time
 
-def main():
-    kots = Kots.from_json_file("../model/sample_robot.json")
-
-    coord = [1., 2., 3.]
-    veloc = [4., 5., 6.]
-    accel = [0., 0., 0.]
-    
-    vec = []
-    vec.extend(coord)
-    vec.extend(veloc)
-    vec.extend(accel)    
-    kots.import_motions(vec)
-    
+def link_jacobian_num(kots, target, order = 3, delta = 1e-8):
     kots.kinematics()
 
-    target = ["arm1","arm2","arm3"]
-    kots.link_jacobian(target,3)
+    x = kots.motions()
+    p0 = kots.state_link_info_list("cmtm", target)
+    
+    row = order * 6
+    col = order * kots.dof()
+    
+    J = np.zeros((row, col))
+    for i in range(col):
+        x_ = x.copy()
+        x_[i] += delta
+        kots.import_motions(x_)
+        kots.kinematics()
+        p1 = kots.state_link_info_list("cmtm", target)
+        for j in range(len(p0)):
+            dp = mr.CMTM.sub_vec(p0[j], p1[j]) / delta
+            J[j*row:(j+1)*row,i] = dp[:row]
+
+    return J
+
+ORDER = 1
+
+def main():
+    kots = Kots.from_json_file("../model/sample_robot.json")
+    motion = np.random.rand(kots.order()*kots.dof())
+    kots.import_motions(motion)
+
+    kots.kinematics()
+
+    target = ["end_effector"]
+    jacob = kots.link_jacobian(target,ORDER)
+
+    jacob_num = link_jacobian_num(kots, target, ORDER)
+    
+    print("jacobian shape: ", jacob.shape)
+    print("jacobian: ", jacob)
+    print("jacobian_num shape: ", jacob_num.shape)
+    print("jacobian_num: ", jacob_num)
+
+    print("norm: ", np.linalg.norm(jacob[:,:(ORDER*kots.dof())] - jacob_num))
   
 if __name__ == "__main__":
     main()
