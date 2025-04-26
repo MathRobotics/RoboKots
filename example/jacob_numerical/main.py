0a1dd12869bb18167a7b6f3cadc6b4bf90a548b8
Taiki Ishigaki <taiki000ishigaki@gmail.com>
2025-04-26 14:19:26 +0900

    📦 NEW:

--- example/jacob_numerical/main.py
+++ example/jacob_numerical/main.py
@@ -2,16 +2,15 @@ import numpy as np
 
 import mathrobo as mr
 from robokots.kots import *
-import time
 
-def link_jacobian_num(kots, target, order = 3, delta = 1e-8):
+def link_jacobian_num(kots, delta = 1e-8):
     kots.kinematics()
 
     x = kots.motions()
-    p0 = kots.state_link_info_list("cmtm", target)
+    p0 = kots.state_target_link_info("frame")
     
-    row = order * 6
-    col = order * kots.dof()
+    row = 6 * len(p0)
+    col = kots.dof()
     
     J = np.zeros((row, col))
     for i in range(col):
@@ -19,33 +18,34 @@ def link_jacobian_num(kots, target, order = 3, delta = 1e-8):
         x_[i] += delta
         kots.import_motions(x_)
         kots.kinematics()
-        p1 = kots.state_link_info_list("cmtm", target)
+        p1 = kots.state_target_link_info("frame")
         for j in range(len(p0)):
-            dp = mr.CMTM.sub_vec(p0[j], p1[j]) / delta
-            J[j*row:(j+1)*row,i] = dp[:row]
+            dp = mr.SE3.sub_tan_vec(p0[j], p1[j]) / delta
+            J[j*6:(j+1)*6,i] = dp
 
     return J
-
-ORDER = 1
 
 def main():
     kots = Kots.from_json_file("../model/sample_robot.json")
     motion = np.random.rand(kots.order()*kots.dof())
     kots.import_motions(motion)
 
+    kots.set_target_from_file("target_list.json")
+
     kots.kinematics()
 
-    target = ["arm3"]
-    jacob = kots.link_jacobian(target,ORDER)
+    jacob = kots.link_jacobian_target(1)
 
-    jacob_num = link_jacobian_num(kots, target, ORDER)
+    jacob_num = link_jacobian_num(kots)
     
     print("jacobian shape: ", jacob.shape)
-    print("jacobian: ", jacob)
     print("jacobian_num shape: ", jacob_num.shape)
-    print("jacobian_num: ", jacob_num)
 
-    print("norm: ", np.linalg.norm(jacob[:,:(ORDER*kots.dof())] - jacob_num))
+    print("norm: ", np.linalg.norm(jacob - jacob_num))
+
+    for i in range(jacob.shape[0]):
+      print(jacob[i])
+      print(jacob_num[i])
   
 if __name__ == "__main__":
     main()
