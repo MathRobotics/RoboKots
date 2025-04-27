68938a56e8516d774e026c7029fad9f34e86b20e
Taiki Ishigaki <taiki000ishigaki@gmail.com>
2025-04-27 23:57:44 +0900

    🐛 FIX:

--- robokots/kinematics.py
+++ robokots/kinematics.py
@@ -38,12 +38,12 @@ def link_rel_frame(joint : JointStruct, joint_coord : np.ndarray) -> SE3:
 
 def link_rel_vel(joint : JointStruct, joint_vel : np.ndarray) -> np.ndarray:
   local_vel = joint_local_vel(joint, joint_vel)
-  rel_vel = joint.origin.mat_inv_adj() @ local_vel
+  rel_vel = local_vel
   return rel_vel
 
 def link_rel_acc(joint : JointStruct, joint_acc : np.ndarray) -> np.ndarray:
   local_acc = joint_local_acc(joint, joint_acc)
-  rel_acc = joint.origin.mat_inv_adj() @ local_acc
+  rel_acc = local_acc
   return rel_acc
 
 def kinematics(joint : JointStruct, p_link_frame : SE3, joint_coord : np.ndarray) -> SE3:
