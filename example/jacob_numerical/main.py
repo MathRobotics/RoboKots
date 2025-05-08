3ff587d5ba7c8932282df044aa7ab662cdfc5091
Taiki Ishigaki <taiki000ishigaki@gmail.com>
2025-05-08 18:20:54 +0900

    👌 IMPROVE:

--- example/jacob_numerical/main.py
+++ example/jacob_numerical/main.py
@@ -3,32 +3,8 @@ import numpy as np
 import mathrobo as mr
 from robokots.kots import *
 
-def link_jacobian_num(kots, delta = 1e-8):
-    kots.kinematics()
-
-    x = kots.motions()
-    p0 = kots.state_target_link_info("frame")
-    
-    row = 6 * len(p0)
-    col = kots.dof()
-    
-    J = np.zeros((row, col))
-    for i in range(col):
-        x_ = x.copy()
-        x_[i] += delta
-        kots.import_motions(x_)
-        kots.kinematics()
-        p1 = kots.state_target_link_info("frame")
-        for j in range(len(p0)):
-            dp = mr.SE3.sub_tan_vec(p0[j], p1[j]) / delta
-            J[j*6:(j+1)*6,i] = dp
-
-    return J
-
-ORDER = 1
-
 def main():
-    kots = Kots.from_json_file("../model/sample_robot.json", order=ORDER)
+    kots = Kots.from_json_file("../model/sample_robot.json")
     motion = np.random.rand(kots.order()*kots.dof())
     kots.import_motions(motion)
 
@@ -38,16 +14,16 @@ def main():
 
     jacob = kots.link_jacobian_target(1)
 
-    jacob_num = link_jacobian_num(kots)
+    jacob_num = kots.link_jacobian_target_numerical("frame")
     
     print("jacobian shape: ", jacob.shape)
     print("jacobian_num shape: ", jacob_num.shape)
 
     print("norm: ", np.linalg.norm(jacob - jacob_num))
 
-    for i in range(jacob.shape[0]):
-      print(jacob[i])
-      print(jacob_num[i])
+    # for i in range(jacob.shape[0]):
+    #     print("jac ana\n", jacob[i])
+    #     print("jac num\n", jacob_num[i])
   
 if __name__ == "__main__":
     main()
