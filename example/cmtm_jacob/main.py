dae628d955630ce7ae50212627ff28521c85c88a
Taiki Ishigaki <taiki000ishigaki@gmail.com>
2025-05-09 13:14:55 +0900

    👌 IMPROVE:

--- example/cmtm_jacob/main.py
+++ example/cmtm_jacob/main.py
@@ -4,32 +4,11 @@ import mathrobo as mr
 from robokots.kots import *
 import time
 
-def link_jacobian_num(kots, target, order = 3, delta = 1e-8):
-    kots.kinematics()
-
-    x = kots.motions()
-    p0 = kots.state_link_info_list("cmtm", target)
-    
-    row = order * 6
-    col = order * kots.dof()
-    
-    J = np.zeros((row * len(p0), col))
-    for i in range(col):
-        x_ = x.copy()
-        x_[i] += delta
-        kots.import_motions(x_)
-        kots.kinematics()
-        p1 = kots.state_link_info_list("cmtm", target)
-        for j in range(len(p0)):
-            dp = mr.CMTM.sub_vec(p0[j], p1[j]) / delta
-            J[j*row:(j+1)*row,i] = dp[:row]
-    return J
-
 ORDER = 3
 
 def main():
     kots = Kots.from_json_file("../model/sample_robot.json")
-    target = ["arm2"]
+    target = ["arm3"]
 
     motion = np.random.rand(kots.order()*kots.dof())
     kots.import_motions(motion)
@@ -41,9 +20,8 @@ def main():
     acceleration = kots.state_link_info_list("acc", target)
     print("acceleration:", acceleration)
 
-
     jacob = kots.link_jacobian(target, ORDER)
-    jacob_num = link_jacobian_num(kots, target, ORDER)
+    jacob_num = kots.link_jacobian_numerical(target, "cmtm")
 
     veloc_jac = jacob[0:6,:2*kots.dof()] @ motion[kots.dof():]
     accel_jac = jacob[6:12,:2*kots.dof()] @ motion[kots.dof():]
