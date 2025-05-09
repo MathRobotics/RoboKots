e221fee391773fefb0f23760c66c255536595581
Taiki Ishigaki <taiki000ishigaki@gmail.com>
2025-05-09 12:53:00 +0900

    👌 IMPROVE:

--- example/cmtm_jacob/main.py
+++ example/cmtm_jacob/main.py
@@ -21,37 +21,46 @@ def link_jacobian_num(kots, target, order = 3, delta = 1e-8):
         kots.kinematics()
         p1 = kots.state_link_info_list("cmtm", target)
         for j in range(len(p0)):
-            dp = mr.CMTM.sub_ptan_vec(p0[j], p1[j]) / delta
-            J[j*row:(j+1)*row,i] = \
-                CMTM.tan_to_ptan(6, p0[j]._n) @ p0[j].tan_mat_inv_adj() @ mr.CMTM.ptan_to_tan(6, order) @ dp[:row]
-
+            dp = mr.CMTM.sub_vec(p0[j], p1[j]) / delta
+            J[j*row:(j+1)*row,i] = dp[:row]
     return J
 
 ORDER = 3
 
 def main():
     kots = Kots.from_json_file("../model/sample_robot.json")
+    target = ["arm2"]
 
     motion = np.random.rand(kots.order()*kots.dof())
     kots.import_motions(motion)
 
     kots.kinematics()
     print("motion:", kots.motions())
-    print("velocity:", kots.state_link_info_list("vel", ["arm3"]))
-    print("acceleration:", kots.state_link_info_list("acc", ["arm3"]))
+    velocity = kots.state_link_info_list("vel", target)
+    print("velocity:", velocity)
+    acceleration = kots.state_link_info_list("acc", target)
+    print("acceleration:", acceleration)
 
-    target = ["arm3"]
+
     jacob = kots.link_jacobian(target, ORDER)
-
     jacob_num = link_jacobian_num(kots, target, ORDER)
 
-    print("vel", jacob[0:6,:6] @ motion[3:])
-    print("acc", jacob[6:12,:6] @ motion[3:])
-    
+    veloc_jac = jacob[0:6,:2*kots.dof()] @ motion[kots.dof():]
+    accel_jac = jacob[6:12,:2*kots.dof()] @ motion[kots.dof():]
+
+    print("velocity", veloc_jac)
+    print("acceleration", accel_jac)
+
+    print("velocity norm: ", np.linalg.norm(velocity - veloc_jac))
+    print("acceleration norm: ", np.linalg.norm(acceleration - accel_jac)) 
+
     print("jacobian_ana shape: ", jacob.shape)
-    print("jacobian: ", jacob[12:])
     print("jacobian_num shape: ", jacob_num.shape)
-    print("jacobian_num: ", jacob_num[12:])
+
+    for i in range(6):
+        print("jacobian: ", jacob[12+i:12+i+1])
+        print("jacobian_num: ", jacob_num[12+i:12+i+1])
+        print("norm: ", np.linalg.norm(jacob[12+i:12+i+1] - jacob_num[12+i:12+i+1]))
 
     print("norm: ", np.linalg.norm(jacob - jacob_num))
 
