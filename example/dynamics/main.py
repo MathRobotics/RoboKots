1b37263ed0d2908559eb5998f68080eec473bc4e
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-06-12 13:57:57 +0900

    👌 IMPROVE:

--- example/dynamics/main.py
+++ example/dynamics/main.py
@@ -1,22 +1,13 @@
 import numpy as np
+import time
 
 from robokots.kots import *
 
 def main():
-  kots = Kots.from_json_file("../model/sample_robot.json")
-  kots.print_structure()
-  
-  link_name_list = ["world","base","arm1","arm2","arm3"]
-  links = kots.link_list(link_name_list)
-  print(list(l.type for l in links) )
-
-  joint_name_list = ["root","joint1","joint2","joint3"]
-  joints = kots.joint_list(joint_name_list)
-  print(list(j.type for j in joints) )
+  kots = Kots.from_json_file("../model/sample_robot.json", order = 5)
   
   motion = np.random.rand(kots.order()*kots.dof())
   kots.import_motions(motion)
-  print(kots.motions())
   
   kots.dynamics()
   
