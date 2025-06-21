3366ed39bf280aae6743ed7d32dcdc65ac027a33
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-06-21 17:08:06 +0900

    🌟ADD:

--- robokots/dynamics/dynamics_matrix.py
+++ robokots/dynamics/dynamics_matrix.py
@@ -4,6 +4,7 @@
 # dynamics computation module by matrix formulation
 
 import numpy as np
+import mathrobo as SO3, SE3, CMTM
 
 def inertia_diag_mat(inertia : np.ndarray, order : int = 1) -> np.ndarray:
     if inertia.shape != (6, 6):
@@ -11,7 +12,23 @@ def inertia_diag_mat(inertia : np.ndarray, order : int = 1) -> np.ndarray:
     
     return np.kron(np.eye(order), inertia)
 
-def  natural_num_diag_mat(order : int = 1) -> np.ndarray:
-    v = np.repeat(np.arange(1, order), 6)
+def  natural_num_diag_mat(order : int = 1, dim : int = 6) -> np.ndarray:
+    '''
+    return diagonal matrix with natural numbers from 1 to order repeated dim times
+    size is (dim * order, dim * order)
+    '''
+    v = np.repeat(np.arange(1, order), dim)
     return np.diag(v)
 
+def momentum_to_force_mat(m : CMTM, order : int = 1, dim : int = 6) -> np.ndarray:
+    momentum_dof = dim * (order+1)
+    force_dof = dim * order
+    mat = np.zeros((force_dof, momentum_dof))
+    
+    mat[:, dim:] = natural_num_diag_mat(force_dof, dim)
+    if dim == 6:
+      mat[:, :force_dof] += -CMTM.hat(SE3, m.tan_vecs(order+1)).T
+    elif dim == 3:
+      mat[:, :force_dof] += -CMTM.hat(SO3, m.tan_vecs(order+1)).T
+      
+    return mat
