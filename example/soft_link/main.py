28c53fc8d0437c41cd251412aa02e4ce22a22a5a
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-07-07 11:51:17 +0900

    👌 IMPROVE:

--- example/soft_link/main.py
+++ example/soft_link/main.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-from robokots.kots import *
+from robokots.kots_soft import *
 
 def main():
   kots = Kots.from_json_file("../model/soft_rod.json")
   kots.print_structure()
+
+  kots.kinematics()
+
+
   
 if __name__ == "__main__":
     main()
