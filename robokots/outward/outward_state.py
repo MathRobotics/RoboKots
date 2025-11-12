e7319dfb1d85c8498aff6bf45957b4ebd4a145b6
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-11-12 20:06:42 +0900

    👌 IMPROVE:

--- robokots/outward/outward_state.py
+++ robokots/outward/outward_state.py
@@ -9,14 +9,14 @@ def outward_state(robot : RobotStruct, state_dict : dict, state_type : StateType
     if state_type.owner_type == "link":
         link_name = state_type.owner_name
     elif state_type.owner_type == "joint":
-        joint = robot.joint_list(state_type.owner_name)
-        link_name = robot.links[joint.parent_link_id].name
+        joint = robot.joint_list([state_type.owner_name])
+        link_name = robot.links[joint[0].parent_link_id].name
 
     if state_type.frame_name == "world":
         if state_type.is_dynamics:
-            cmtm_wrench = state_dict_to_cmtm_wrench(state_dict, link_name, state_type.order)
+            cmtm_wrench = state_dict_to_cmtm_wrench(state_dict, link_name, state_type.key_order)
         else:
-            cmtm = state_dict_to_cmtm(state_dict, link_name, state_type.order)
+            cmtm = state_dict_to_cmtm(state_dict, link_name, state_type.key_order)
 
     if state_type.data_type == "frame":
         return state_dict_to_frame(state_dict, state_type.owner_name)
@@ -26,8 +26,8 @@ def outward_state(robot : RobotStruct, state_dict : dict, state_type : StateType
         if state_type.frame_name == 'world':
             local_momentum = state_dict_to_cmvec(state_dict, state_type.owner_name, \
                                                  state_type.owner_type+"_momentum", \
-                                                 state_type.order).cm_vec()
-            world_momentum = CMVector((cmtm_wrench.mat_adj() @ local_momentum).reshape(-1,6)).vecs()
+                                                 state_type.key_order).cm_vec()
+            world_momentum = CMVector((Factorial.mat(state_type.key_order, dim=6) @ cmtm_wrench.mat_adj() @ local_momentum).reshape(-1,6)).vecs()
             return world_momentum[-1]
         else:
             return np.array(state_dict[state_type.alliance])
@@ -35,8 +35,8 @@ def outward_state(robot : RobotStruct, state_dict : dict, state_type : StateType
         if state_type.frame_name == 'world':
             local_force = state_dict_to_cmvec(state_dict, state_type.owner_name, \
                                                 state_type.owner_type+"_force", \
-                                                state_type.order).cm_vec()
-            world_force = CMVector((cmtm_wrench.mat_adj() @ local_force).reshape(-1,6)).vecs()
+                                                state_type.key_order).cm_vec()
+            world_force = CMVector((Factorial.mat(state_type.key_order, dim=6) @ cmtm_wrench.mat_adj() @ local_force).reshape(-1,6)).vecs()
             return world_force[-1]
         else:
             return np.array(state_dict[state_type.alliance])
