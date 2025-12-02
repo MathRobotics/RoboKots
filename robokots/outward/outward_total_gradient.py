a41b4138da8d681c3ea53fb972952af540888fd7
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-12-02 17:03:11 +0900

    ‼️ BREAKING: delete previous jacobian numerical

--- robokots/outward/outward_total_gradient.py
+++ robokots/outward/outward_total_gradient.py
@@ -189,38 +189,3 @@ def outward_jacobian(robot : RobotStruct, state : dict, state_type_list : list[S
                 raise ValueError("torque can be specified only for joint owner type")
             jacobs = np.vstack((jacobs, jacob_part))
     return jacobs
-
-def dynamics_jacobian_numerical(robot : RobotStruct, motions : RobotMotions, link_name_list : list[str], data_type, owner_type, frame_name : str = None, output_order_ : int = 1) -> np.ndarray:
-    order = keys_time_order[data_type] + output_order_ - 1
-    dynamics_time_order = keys_time_order[data_type] + output_order_ - keys_time_order["force"]
-
-    if dynamics_time_order < 1:
-        dynamics_time_order = 0
-    d = outward_state_dof(robot, StateType(owner_type, link_name_list[0], data_type, frame_name), dim = 3)
-    dof = d * output_order_
-    
-    jacobs = np.zeros((dof*len(link_name_list),robot.dof*order))
-    motion = np.zeros(robot.dof * order)
-
-    for joint in robot.joints:
-        m = motions.joint_motions(joint.dof, joint.dof_index, order)
-        motion[joint.dof_index*order:joint.dof_index*order+joint.dof*order] = m.flatten()
-    
-    for link in robot.links:
-        m = motions.link_motions(link.dof, link.dof_index, order)
-        motion[link.dof_index*order:link.dof_index*order+link.dof*order] = m.flatten()
-
-    for i in range(len(link_name_list)):
-        def dynamics_func(x):
-            state = outward_dynamics(robot, x, dynamics_order=dynamics_time_order)
-            y = np.zeros(dof)
-            for j in range(output_order_):
-                if j == 0:
-                    y[d*j:d*(j+1)] = outward_state(robot, state, StateType(owner_type, link_name_list[i], data_type, frame_name))
-                else:
-                    y[d*j:d*(j+1)] = outward_state(robot, state, StateType(owner_type, link_name_list[i], data_type+"_diff"+str(j), frame_name))
-            return y
-
-        jacobs[dof*i:dof*(i+1)] = numerical_grad(motion, dynamics_func)
-
-    return jacobs
