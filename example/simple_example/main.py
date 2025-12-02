d546d5d472200cf0afad5f12174be6bacc30e671
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2025-12-02 16:39:21 +0900

    👌 IMPROVE:

--- example/simple_example/main.py
+++ example/simple_example/main.py
@@ -2,51 +2,53 @@ import numpy as np
 
 import mathrobo as mr
 from robokots.kots import *
+from robokots.outward import outward_gradient
+from robokots.outward import outward
 
 def main():
     kots = Kots.from_json_file("../model/sample_robot.json")
     kots.print_structure()
 
     kots.set_target_from_file("target_list.json")
-    kots.print_targets()
+    print(kots.targets())
+
+    kots.set_order(6)
 
     motion = np.random.rand(kots.order()*kots.dof())
     kots.import_motions(motion)
 
     kots.kinematics()
 
-    jacob = kots.jacobian(["end"], "frame")
-    jacob_num = kots.jacobian_numerical(["end"], "frame")
+    state = StateType('link','end','frame')
+    jacob = kots.jacobian(state)
+    jacob_num =  kots.jacobian(state, numerical=True)
     print("norm: ", np.linalg.norm(jacob - jacob_num))
 
-    jacob = kots.jacobian_target() 
-    jacob_target = kots.jacobian_target([["vel", "acc", "jerk", "snap", "force"]])
-    print("norm: ", np.linalg.norm(jacob - jacob_target))
-
-    jacob = kots.jacobian(["end"], "force")
-    jacob_num = kots.jacobian_numerical(["end"], "force")
+    state = StateType('link','end','snap')
+    jacob = kots.jacobian(state)
+    jacob_num =  kots.jacobian(state, numerical=True)
     print("norm: ", np.linalg.norm(jacob - jacob_num))
 
-    fk_vel = kots.state_target_link_info('vel')
-    jacob_vel = kots.jacobian_target("frame")@kots.motion_diff(1)
+    state_list = StateType.create_list('link','end',["vel","acc","jerk","snap"])
+    jacob = kots.jacobian(state_list)
+    jacob_num = kots.jacobian(state_list, numerical=True)
+    print("norm: ", np.linalg.norm(jacob - jacob_num))
 
-    for i in range(len(fk_vel)):
-      print(fk_vel[i])
-      print(jacob_vel[6*i:6*(i+1)])
-
-    fk_acc = kots.state_target_link_info('acc')
-    jacob_acc = kots.jacobian_target("vel")@kots.motion_diff(2)
-      
-    print(fk_acc[0])
-    print(jacob_acc)
-    
     kots.dynamics()
 
-    print(kots.target_info())
-  
-    kots.show_robot()
+    st_list = StateType.create_list('link','end',["momentum_diff3", "force_diff2"])
+    print("norm: ", np.linalg.norm( kots.jacobian(st_list) -  kots.jacobian(st_list, numerical=True) ))
 
-    print(kots.jacobian_target("force"))
+    st_list = StateType.create_list('link','end',["momentum_diff3"], "world")
+    print("norm: ", np.linalg.norm( kots.jacobian(st_list) -  kots.jacobian(st_list, numerical=True) ))
+
+    st_list = StateType.create_list('joint','joint4',["momentum_diff3"], "world")
+    print("norm: ", np.linalg.norm( kots.jacobian(st_list) -  kots.jacobian(st_list, numerical=True) ))
+    
+    st_list = StateType.create_list('joint','joint4',["momentum_diff3", "force_diff2", "torque_diff2"])
+    print("norm: ", np.linalg.norm( kots.jacobian(st_list) -  kots.jacobian(st_list, numerical=True) ))
+
+    kots.show_robot()
 
 if __name__ == "__main__":
     main()
